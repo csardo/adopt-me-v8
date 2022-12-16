c15684d8dad5505f5e2e3061b8e3344a994da960
csardo <csardooo@gmail.com>
2022-12-15 19:57:32 -0800

    reset to uncontrolled forms

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,24 +1,23 @@
-import { Link } from 'react-router-dom';
-// use Link instead of a tag in SPAs, but note anytime you use a Link tag it has to be within a BrowserRouter component
+import { Link } from "react-router-dom";
 
-const Pet = ({ name, animal, breed, images, location, id }) => {
-    let hero = "http://pet-images.dev-apis.com/pets/none.jpg";
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
 
-    if (images.length) {
-        hero = images[0];
-    }
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
 
   return (
-      <Link to={`/details/${id}`} className="pet">
-          <div className="image-container">
-              <img src={hero} alt={name} />
-          </div>
-          <div className="info">
-              <h1>{name}</h1>
-              <h2>{animal} - {breed} - {location}</h2>
-          </div>
-      </Link>
-      
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </Link>
   );
 };
 
